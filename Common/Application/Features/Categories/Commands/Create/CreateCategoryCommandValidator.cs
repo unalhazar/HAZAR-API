468e33225771ae81cf7c917b7f3a1a66c4fc3e08
Ünal Hazar <unalhazarr@gmail.com>
2024-08-26 14:55:52 +0300

    validation

---
+++ Common/Application/Features/Categories/Commands/Create/CreateCategoryCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Features.Categories.Commands.Create
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(10).WithMessage("Name cannot exceed 10 characters.");
+        }
+    }
+}
---
+++ Common/Application/Features/Products/Commands/Create/CreateProductCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Features.Products.Commands.Create
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(10).WithMessage("Name cannot exceed 10 characters.");
+        }
+    }
+}
